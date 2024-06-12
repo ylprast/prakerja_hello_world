89d079b4f6c524e63fb5f4d7289a09d77ce7fcb7
Yulianaprast <yulianaprast37@gmail.com>
2024-06-12 10:11:01 +0700

    add js code for temp, date&clock, form validation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -30,4 +30,85 @@
   
   })(window.jQuery);
 
+//Konversi suhu fahrenheit ke celcius
+  function convertToFahrenheit() {
+    const fahrenheitInput = document.getElementById('fahrenheit').value;
+    if (fahrenheitInput === "") {
+        alert("Masukkan suhu dalam Fahrenheit!");
+        return;
+    }
 
+    const fahrenheit = parseFloat(fahrenheitInput);
+    const celsius = (fahrenheit - 32) * 5 / 9;
+
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = `${fahrenheit} Fahrenheit = ${celsius.toFixed(2)} Celsius`;
+}
+
+//Validasi Form Email dan Nomor Telepon
+function validateForm() {
+  const emailInput = document.getElementById('formemail').value;
+  const phoneInput = document.getElementById('formphone').value;
+
+  let emailValid = validateEmail(emailInput);
+  let phoneValid = validatePhone(phoneInput);
+
+  if (emailValid && phoneValid) {
+      alert("Yeay! Your feedback is already sent! Thank you^^");
+  }
+}
+
+function validateEmail(email) {
+  const emailErrorElement = document.getElementById('emailError');
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!email) {
+      emailErrorElement.textContent = "Uh oh, Email cant be empty.";
+      return false;
+  } else if (!emailPattern.test(email)) {
+      emailErrorElement.textContent = "Invalid email format. Let's try again.";
+      return false;
+  } else {
+      emailErrorElement.textContent = "";
+      return true;
+  }
+}
+
+function validatePhone(phone) {
+  const phoneErrorElement = document.getElementById('phoneError');
+
+  if (!phone) {
+      phoneErrorElement.textContent = "Oops, Phone number cant be empty.";
+      return false;
+  } else if (!/^\d{12,}$/.test(phone)) {
+      phoneErrorElement.textContent = "Phone number must be at least 12 digits. Let's do some recheck.";
+      return false;
+  } else {
+      phoneErrorElement.textContent = "";
+      return true;
+  }
+}
+
+//Kalender & Jam Realtime
+function updateClock() {
+  const clockElement = document.getElementById('clock');
+  const now = new Date();
+
+  const day = String(now.getDate()).padStart(2, '0');
+  const month = String(now.getMonth() + 1).padStart(2, '0');
+  const year = now.getFullYear();
+
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+
+  const formattedTime = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+
+  clockElement.textContent = formattedTime;
+}
+
+//Mengupdate jam perdetik
+setInterval(updateClock, 1000);
+
+//Mengupdate jam secara realtime
+updateClock();
